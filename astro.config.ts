205c6f90312242b2b02679658e5e9168a922ef78
ryoma-abe <liaozhenanbu387@gmail.com>
2025-01-25 19:25:07 +0900

    デプロイ

--- astro.config.ts
+++ astro.config.ts
@@ -2,11 +2,9 @@ import { defineConfig } from 'astro/config'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 
-// https://astro.build/config
 export default defineConfig({
-  site: process.env.CI
-    ? 'https://astro-shadcn-ui-template.vercel.app'
-    : 'http://localhost:4321',
+  site: 'https://ryoma-abe.github.io',
+  base: '/astro-portfolio',
   integrations: [
     react(),
     tailwind({
